bcbdd2b3237714576a7f864eae926601a106a106
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-11-15 15:24:41 +0100

    Update Podfile and app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,28 @@
-import 'package:eden_task/cores/constants/colors.dart';
-
-import '../features/auth/presentation/pages/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../cores/constants/colors.dart';
+import '../cores/navigator/app_router.dart';
+import '../cores/navigator/router_generator.dart';
+import '../features/auth/presentation/pages/login_view.dart';
 
 class EdenApp extends StatelessWidget {
   const EdenApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: AppColor.primaryColor,
-      ),
-      initialRoute: LoginView.routeName,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, Widget? child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(primaryColor: AppColor.primaryColor),
+          initialRoute: LoginView.routeName,
+          onGenerateRoute: RouteGenerator.generateRoute,
+          navigatorKey: AppRouter.instance.navigatorKey,
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
